b2dca3e57f33b33e586fc9612aaecac3c9bbcfc5
brinkqiang <brink.qiang@gmail.com>
2018-10-25 11:52:11 +0800

    add

--- test/dmdumptest.cpp
+++ test/dmdumptest.cpp
@@ -1,9 +1,18 @@
 
 #include "dmdump.h"
+#include "dmflags.h"
+
 #include <iostream>
 
-int main( int argc, char* argv[] ) {
+DEFINE_uint64(AGE, 23, "age");
+DEFINE_string(NAME, "andy", "name");
+
+// dmflagstest --AGE=36 --NAME=tom
+int main(int argc, char **argv) 
+{   
+	DMFLAGS_INIT(argc, argv);
     std::string strResult = exec("ps -aux");
-    std::cout << strResult;
-    return 0;
+	std::cout << "AGE = " << FLAGS_AGE << std::endl;
+	std::cout << "NAME = " << FLAGS_NAME << std::endl;
+	return 0;   
 }
