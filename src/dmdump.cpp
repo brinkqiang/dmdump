e583d44630b2f9c6d250f0983323a442dc591df9
brinkqiang <brink.qiang@gmail.com>
2018-10-25 13:03:08 +0800

    add

--- src/dmdump.cpp
+++ src/dmdump.cpp
@@ -118,7 +118,7 @@ std::vector<uint64_t> DMGetProcessList(const std::string& strName)
     strtk::std_string::token_list_type::iterator it = token_list.begin();
     for (; it != token_list.end(); ++it)
     {
-        vecList.push_back(atoll(it->second));
+        vecList.push_back(atoll(it->first));
     }
 
     return vecList;
